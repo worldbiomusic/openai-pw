8fdc9c9f0c380c195d27231f1aa62c2caf9e6f2b
worldbiomusic <worldbiomusic@gmail.com>
2023-08-12 23:03:03 +0900

    Linked audio libraries
    - Audio bot can now use whisper and whisper-timestamped library

--- opaw/examples/audio.py
+++ opaw/examples/audio.py
@@ -13,15 +13,19 @@ logger = log.get("audio", "logs/audio.log")
 bot = AudioBot()
 
 # transcript (language: https://en.wikipedia.org/wiki/List_of_ISO_639-1_codes)
-response = bot.create("audio1.mp3", task="stt", language="en")
-logger.info(f"stt: {response['text']}")
+file = "radio_short.mp3"
 
-# translation
-response = bot.create("audio1.mp3", task="mt", language="en")
-logger.info(f"mt: {response['text']}")
+# official api
+response = bot.create(file, lib="api", task="stt")
+logger.info(f"official api: {response['text']}")
+
+# whisper
+response = bot.create(file, lib="whisper", name="tiny")
+logger.info(f"whisper: {response}")
+
+# whisper_timestamped
+response = bot.create(file, lib="whisper_t", name="tiny", device="cpu")
+logger.info(f"whisper_timestamped: {util.pprints(response)}")
 
 # save history if needed
 bot.save_history("history/audio-hist.json")
-
-
-
